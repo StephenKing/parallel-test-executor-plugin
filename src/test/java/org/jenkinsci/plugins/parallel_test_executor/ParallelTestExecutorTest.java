e7edbea7c201081d29a1d40a83a6f40c1d9feb1b
Steffen Gebert <steffen.gebert@emnify.com>
2017-08-07 15:12:30 +0200

    [JENKINS-46028] Use primary job as fallback
    Currently, test results are only taken from the last builds of this job. The
    result is that the first build(s) of a newly created branch (when using multi-
    branch jobs) do not run tests in parallel, unless the build for this branch is
    non-FAILURE. With short-lived feature branches, this results in unsatisfying
    long build times.
    
    In case of multi-branch jobs, other branches could act as a source for test
    records as well. The question, which branch to use as reference can be answered
    with the SCM's help - the PrimaryInstanceMetadataAction is assigned by several
    SCM plugins and denotes one/multiple branches as "primary" (in Git: the default
    branch when checking out).

--- src/test/java/org/jenkinsci/plugins/parallel_test_executor/ParallelTestExecutorTest.java
+++ src/test/java/org/jenkinsci/plugins/parallel_test_executor/ParallelTestExecutorTest.java
@@ -1,21 +1,28 @@
 package org.jenkinsci.plugins.parallel_test_executor;
 
 import hudson.model.FreeStyleProject;
+import jenkins.branch.BranchSource;
+import jenkins.plugins.git.GitSCMSource;
+import jenkins.plugins.git.GitSampleRepoRule;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.cps.SnippetizerTester;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.recipes.LocalData;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ParallelTestExecutorTest {
 
     @Rule
     public JenkinsRule jenkinsRule = new JenkinsRule();
+    @Rule
+    public GitSampleRepoRule sampleRepo = new GitSampleRepoRule();
 
     @Test
     @LocalData
@@ -52,4 +59,43 @@ public class ParallelTestExecutorTest {
         jenkinsRule.assertLogContains("splits[1]: includes=true list=[two.java, two.class]", b2);
     }
 
+
+    @Test
+    public void multiBranchFallbackToPrimaryJob() throws Exception {
+
+        // create a Jenkinsfile in the master branch
+        sampleRepo.init();
+        String script =
+                "def splits = splitTests parallelism: count(2), generateInclusions: true\n" +
+                "echo \"branch=${env.BRANCH_NAME}\"\n" +
+                        "node {\n" +
+                        "  checkout scm\n" +
+                        "}";
+        sampleRepo.write("Jenkinsfile", script);
+        sampleRepo.git("add", "Jenkinsfile");
+        sampleRepo.git("commit", "--all", "--message=flow");
+        // create a new branch based on master
+        sampleRepo.git("branch", "dev/main");
+        // checkout a new branch that will get PrimaryInstanceMetadataAction because of it is checked out when indexing
+        sampleRepo.git("checkout", "-b", "primary-branch");
+
+        // create MultiBranch project "p"
+        WorkflowMultiBranchProject mp = jenkinsRule.jenkins.createProject(WorkflowMultiBranchProject.class, "p");/**/
+        mp.getSourcesList().add(new BranchSource(new GitSCMSource(null, sampleRepo.toString(), "", "*", "", false)));
+        // indexing will automatically trigger a run for every branch
+        mp.scheduleBuild2(0).getFuture().get();
+        WorkflowJob p = mp.getItem("dev%2Fmain");
+        // MultiBranch project should have 3 items (master, dev/main, primary-branch)
+        assertEquals(3, mp.getItems().size());
+        jenkinsRule.waitUntilNoActivity();
+        WorkflowRun b1 = p.getLastBuild();
+        assertEquals(1, b1.getNumber());
+
+        // trigger dev/main job again (to be sure that primary-branch ran once before)
+        p.scheduleBuild2(0);
+        jenkinsRule.waitUntilNoActivity();
+        WorkflowRun b2 = p.getLastBuild();
+        assertEquals(2, b2.getNumber());
+        jenkinsRule.assertLogContains("Scanning primary project for test records. Starting with build p/primary-branch #1", b2);
+    }
 }
